f9ee8b896bea4c58ffdb9a3a0752ec4c5b68132e
behrooz khan <kbalochkhan67@gmail.com>
2024-03-30 00:58:35 +0500

    Lecture 3 Content

---
+++ lecture3/app.js
@@ -0,0 +1,45 @@
+// concatination || prompt  ||
+
+
+// console.log("behrooz" + " " + "khan");
+
+
+// var firstName = "behrooz";
+// var lastName = "Khan";
+// var fullName  = firstName + + lastName
+// console.log("Greeting Mr." + fullName);
+
+
+
+// alert("hello world");
+
+// var userName = prompt("enter your name");
+// console.log(userName);
+    // 10            prompt geeting return value into the String 
+// var enterNumber  = prompt("Enter a number!");
+// console.log(enterNumber + 10);  // Asad 20         ||   Behrooz Khan Give Option:    20  22  1010  1050  101010
+// console.log("10" + 10);   // +  Addition   + do string value add krte 
+
+// console.log("20" + 10); 
+
+          //"2 plus to equal + 2 "  - 2   number ke andar se minus krrhe ho string ko 
+// console.log("2 plus to equal" + 2 - 2);   // return NAN  Not A Number 
+
+// console.log(2 -2);
+
+
+var userTable  = +prompt("Enter A Table Name!");
+            // 2      +  "x"  + 1 +  " = " + 2   * 1
+var initialNumber = 1;
+            //2                   1                          1             * 2
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+                                    //   2                      2          *   2  = 4
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+console.log(userTable + " x " + initialNumber  + " = " + initialNumber++ * userTable);
+console.log(userTable + " x " + initialNumber   + " = " +initialNumber++ * userTable);
