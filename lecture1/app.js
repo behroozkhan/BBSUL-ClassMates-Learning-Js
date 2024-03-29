05b0268bbde24bb2454c3ab53573a48b2c81b5a6
behrooz khan <kbalochkhan67@gmail.com>
2024-03-30 00:58:03 +0500

    Lecture 1 Content

---
+++ lecture1/app.js
@@ -0,0 +1,14 @@
+// alert("Hello Behrooz");
+
+// js number || integer
+var age = 20;
+// console.log()
+// alert("Hello World");
+
+
+// string "" ''  chracter or 123behrooz
+
+
+// var originalNum = "23";
+// var newNum = originalNum + 7;
+
