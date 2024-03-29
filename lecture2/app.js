3331030df51e1b5166fd0996d44c2c1c6dc19ef3
behrooz khan <kbalochkhan67@gmail.com>
2024-03-30 00:58:19 +0500

    Lecture 2 Content

---
+++ lecture2/app.js
@@ -0,0 +1,125 @@
+// previous lecture
+// alert 
+// number 
+// String
+
+
+// Today Lecture 
+// Arithmetic Opearator 
+// prompt  
+
+// +  -   *   / %
+
+
+
+// alert(1+1); 
+
+// Addition
+// var num1 = 10;
+// var num2 = 10;
+// console.log(num1 + num2);
+
+
+// Sub
+// var num1 = 10;
+// var num2 = 10;
+// console.log(num1 - num2);  // 0
+
+
+// mutiplication
+// var num1 = 10;
+// var num2 = 10;
+// console.log(num1 * num2);  // 100
+
+
+// Divission
+// var num1 = 10;
+// var num2 = 10;
+// console.log(num1 / num2);  // 1
+
+
+
+// Modulus
+// var num1 = 10;
+// var num2 = 10;
+// console.log(num1 % num2);  // 0
+
+           // 4   8   
+//         //    Bodmas  
+// var num = (2 + 2) * 2 - 4; // 4
+// console.log(num);
+
+// var num = (2 + 2) * 2 % 4; // 4
+// console.log(num);
+
+
+// post increament    Bad me add krna
+// var num = 5;
+// console.log(num++);   // 5  6 
+
+// num++
+// post increament
+// farhan++
+
+
+
+// post decreament
+// var num = 5;
+// console.log(num--);   //  5
+
+
+// pree increamnt 
+// var num = 5;
+// console.log(++num);   //  6 
+
+
+
+// // pree decreament
+// var num = 5;
+// console.log(--num);  // 4   4
+
+
+
+// var num1 = 5;  // 6
+// var num2 = 4;
+
+//           // 6     +  6  || 12     -   3    +   3  || 0
+// console.log(++num1 + num1++  - --num2 + num2--);   // asad 4   ,,,   farhan 0 || 1
+
+
+
+// var num1 = 7; 
+// var num2 = 3; // 3 - 1  = 2
+
+//             // 2  -   2  = 0      +  7      -   8 +1 = 9   = -2
+// // console.log(--num2 -  num2++  + num1++  -   ++num1 );   // farhan ka he 0  our Asad ka he -2
+
+
+
+// var num1 = 5;
+// // 1 - pree increament || 2 - post increament  3 -  ke pre decreament
+// var num2 = --num1;
+// console.log(num2);   //  Asad kha 6 Or Farhan Ne Bhi Kha 6   // 5   // 4
+
+
+// num = 1 = num 1 +1 = 2  delay
+// num++   increament means barna   || post means aage jake is me value bare ge
+
+// ++num    abhi barao  1 = 2  directly   pre pehle kro
+
+// num = 1 + 1 =  2
+
+
+// ++   increament    pehlle barao ya fir bad me baro
+// -- decreament        pehle gtao  ya fir bad me gatao
+
+// post increament   
+// farhan++    =   bad me add hoga
+// pre increament
+// ++farhan   = direct add hoga
+
+
+// farhan-- = bad me kam hogi
+
+// --farhan = direct km hojye ge
+
